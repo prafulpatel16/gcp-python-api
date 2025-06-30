abac9fb1236a2a7bb9528037ff61b67851f45d3f
prafulpatel16 <praful1611@outlook.com>
2025-06-29 19:04:31 -0600

    fixed lint error in main

--- app/main.py
+++ app/main.py
@@ -19,12 +19,11 @@ async def redirect_to_helloworld():
 @app.get("/helloworld", response_class=JSONResponse)
 async def read_root(request: Request):
     """Root endpoint returning JSON status"""
-    logger.info(f"/helloworld hit from {request.client.host}")
+    logger.info("/helloworld hit from %s", request.client.host)
     return {"status": "OK"}
 
 @app.get("/healthz", response_class=JSONResponse)
 async def health_check(request: Request):
     """Returns health check status"""
-    logger.info(f"/healthz hit from {request.client.host}")
+    logger.info("/healthz hit from %s", request.client.host)
     return {"status": "healthy"}
-    
